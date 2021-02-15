a2db0c9b4e5d7f72089a87413d6042f953896fbd
Joost Reus <JoostReus@users.noreply.github.com>
2021-02-15 21:07:48 +0100

    Added scrolling animation through AOS library

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', function () {
-    let top  = window.pageYOffset + window.innerHeight,
-        isVisible = top > document.querySelector('section').offsetTop;
-  
-     if (isVisible) {
-       document.querySelector('section').classList.add('animate');
-     }
-  });
